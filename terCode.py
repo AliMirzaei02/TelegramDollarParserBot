d0d45458bba3536b840317a670eb80720beab002
YourUserNameOnGithubAliMirzaei2002 <mr.ali.mirzaei.02@gmail.com>
2023-06-18 17:48:51 +0330

    Add comment

--- terCode.py
+++ terCode.py
@@ -1,12 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Making a GET request
 url = "https://www.tgju.org/currency"
 #headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
 response = requests.get(url)#, headers=headers)
 
 if response.status_code == 200:
+    # Parsing the HTML
     soup = BeautifulSoup(response.content, "html.parser")
+    # Finding $ price
     dollar_info = soup.find("tr", {"data-market-row":"price_dollar_rl"})
     dollar_dt = dollar_info.find_all("td")
     dollar_pr = dollar_dt[0].text
